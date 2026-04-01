bfb18fec34ed9a9b98834ba2891f737157d7b575
Joshua Farmer <rhubarbjosh@outlook.com>
2026-04-01 21:26:30 +0100

    simply echo

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -9,6 +9,7 @@
 void kmain(void)
 {
         static char Message[] = " [Info] Found tty0 VNode Successfully\r\n";
+        static char InputString[16];
         VNode *tty0;
         Trace(ArchInitialise());
         Trace(SerialInit());
@@ -16,6 +17,8 @@ void kmain(void)
         Trace(VFSCreateDevices());
         Trace(tty0 = RootVNode()->RelativeFind(RootVNode(), "/dev/tty0", 9));
         tty0->WriteFunction(Message, sizeof(Message), 1, tty0);
+        tty0->ReadFunction(InputString, sizeof(InputString) - 1, 1, tty0);
+        tty0->WriteFunction(InputString, sizeof(InputString) - 1, 1, tty0);
         Panic(PANIC_TODO);
         while(1);
 }
