1a72fd389621899399de620926d874e89462e31b
Joshua Farmer <rhubarbjosh@outlook.com>
2026-04-02 14:12:45 +0100

    nvm i have a better idea

--- src/riscv/drivers/serial.h
+++ src/riscv/drivers/serial.h
@@ -3,13 +3,7 @@
 
 #include <common/serial.h>
 
-// not always the same
-#ifdef __ARCH_OVERRIDE_SERIAL_ADDRESS
-        #define SERIAL_PORT __ARCH_OVERRIDE_SERIAL_ADDRESS
-#else
-        #define SERIAL_PORT 0x10000000   // Example RISC-V UART base address
-#endif
-
+#define SERIAL_PORT 0x10000000   // Example RISC-V UART base address
 #define SERIAL_IER   (SERIAL_PORT + 1)  // Interrupt Enable Register
 #define SERIAL_LCR   (SERIAL_PORT + 3)  // Line Control Register
 #define SERIAL_LSR   (SERIAL_PORT + 5)  // Line Status Register
