a924287450c3ddca1393ef64b42dd3a5f1d49f9c
Joshua Farmer <rhubarbjosh@outlook.com>
2026-04-21 12:43:46 +0100

    cd => hdd

--- src/ia32/run.sh
+++ src/ia32/run.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 clear
-mkdir bin/isodir
+mkdir -p bin/isodir
+
 if grub-file --is-x86-multiboot2 bin/unarddegos_ia32.o; then
-        echo the file is multiboot
-        mkdir -p bin/isodir/boot/grub
-        cp bin/unarddegos_ia32.o bin/isodir/boot
-        cp bin/modules bin/isodir/boot -rf
-        cp src/ia32/grub.cfg bin/isodir/boot/grub/grub.cfg
-        grub-mkrescue -o bin/unarddegos_ia32.iso bin/isodir
-        qemu-system-x86_64 -debugcon stdio -cdrom bin/unarddegos_ia32.iso -m 64 -boot d
+    echo "the file is multiboot"
+    mkdir -p bin/isodir/boot/grub
+    cp bin/unarddegos_ia32.o bin/isodir/boot
+    cp -rf bin/modules bin/isodir/boot
+    cp src/ia32/grub.cfg bin/isodir/boot/grub/grub.cfg
+    
+    # Create HDD image instead of ISO
+    dd if=/dev/zero of=bin/unarddegos_ia32.hdd bs=1M count=64
+    
+    # Partition and format (using parted and mkfs)
+    parted -s bin/unarddegos_ia32.hdd mklabel msdos
+    parted -s bin/unarddegos_ia32.hdd mkpart primary fat32 1MiB 100%
+    
+    # Setup loop device WITH PARTITION SCANNING
+    LOOP=$(sudo losetup --show -f --partscan bin/unarddegos_ia32.hdd)
+    echo "Loop device: ${LOOP}"
+    echo "Partition: ${LOOP}p1"
+    
+    # Format the partition
+    sudo mkfs.fat -F32 "${LOOP}p1"
+    
+    # Mount
+    sudo mount "${LOOP}p1" /mnt
+    
+    # Install GRUB to HDD
+    sudo grub-install --target=i386-pc --root-directory=/mnt --boot-directory=/mnt/boot ${LOOP}
+    
+    # Copy kernel and modules
+    sudo mkdir -p /mnt/boot/grub
+    sudo cp bin/unarddegos_ia32.o /mnt/boot/
+    sudo cp -rf bin/modules /mnt/boot/
+    sudo cp src/ia32/grub.cfg /mnt/boot/grub/
+    
+    # Cleanup
+    sudo umount /mnt
+    sudo losetup -d ${LOOP}
+    
+    # Run QEMU with HDD
+    qemu-system-x86_64 -debugcon stdio -hda bin/unarddegos_ia32.hdd -m 64
 else
-        echo the file is not multiboot
+    echo "the file is not multiboot"
 fi
